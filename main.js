bfde7ab5941ac161cf9b24740f746c9e8d7bde8c
ROYGBIV <royston.phetmyxay24@student.cart.org>
2025-02-12 14:02:06 -0800

    uhh notes

---
+++ main.js
@@ -0,0 +1,33 @@
+// organization an dstoring of data is essential to programming
+// one way to organize = arrays
+
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn Kubernetes'];
+
+// arrays = lists that can store any data type
+
+// arrays are ORDERED (numbered positions)
+// array can be created by using array literal, wrapping items in square brackets
+// arrays can store all same data type or several different ones
+
+// array syntax:
+// represented by square brackets and inside content
+// each item is an element
+// each element is separated by commas
+
+const hobbies = ['guitar', 'drawing', 'writing'];
+
+// numbered position is called index
+// access items using their index
+// JS arrays are zero-indexed (start at 0)
+
+let listItem = hobbies[2];
+
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+
+seasons[3] = 'Autumn';
+
+newYearsResolutions[0] = 'Increase programming skills';
+
+newYearsResolutions[1] = 'Get a summer job';
+
+newYearsResolutions[2] = 'Learn 3d modeling';
