2248b5239841dd77d614afb550a63d49645692e0
ROYGBIV <royston.phetmyxay24@student.cart.org>
2025-02-13 14:31:18 -0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- arrays-lesson.js
+++ arrays-lesson.js
@@ -31,3 +31,44 @@ newYearsResolutions[0] = 'Increase programming skills';
 newYearsResolutions[1] = 'Get a summer job';
 
 newYearsResolutions[2] = 'Learn 3d modeling';
+
+// variables declared with const can't be reassigned
+// elements in array declared with const are mutable
+// can change the contents of the array, but cannot assign a new array or value
+
+let condiments = ['Ketchup', 'Mustard', 'Relish'];
+
+condiments[0] = 'Mayo';
+
+condiments = ['Mayo'];
+
+const utensils = ['Fork', 'Spoon', 'Knife'];
+
+utensils[2] = 'Spork';
+
+// we can change fork, spoon, spork, etc. to whatever BUT 'utensils' cannot be redefined
+// last item of array = length minus one
+
+const itemTracker = [];
+itemTracker.push(2,7,6,3);
+
+// .push() adds items to the end of an array
+// .push() is a function
+// ^^^ will mutate (change) array
+// .push() is a destructive method because of this
+// MAKE COPIES!!!!!!!!!!!!!!!!
+
+const chores = [];
+
+chores.push('Ska ten hours', 'Deny, defend, and depose');
+
+// .pop() removes the last item from the end of an array
+// does not take arguments
+// returns the value of the last element
+// also destructive
+
+chores.pop()
+
+
+// remove from beginning: .shift()
+// add to beginning: .unshift()
